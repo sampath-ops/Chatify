8fb6a25f1e84ef122f730c78e0087ddde6c8607b
sampath-ops <sampathkaali002@gmail.com>
2024-07-18 00:56:35 +0530

    server and client intialized for chatify

---
+++ server/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const app = express();
+require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
+
+//GET CONNECTION STRING
+const DB = process.env.MONGO_URL.replace(
+    '<PASSWORD>',
+    process.env.DATABASE_PASSWORD
+  );
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB Connection Successfull");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.listen(process.env.PORT, () =>
+  console.log(`Server started on ${process.env.PORT}`)
+);
