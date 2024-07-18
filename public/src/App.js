70935aaa37b9ad403847a94deb1a0f593de773d1
sampath-ops <sampathkaali002@gmail.com>
2024-07-18 12:58:07 +0530

    Register page styles added

--- public/src/App.js
+++ public/src/App.js
@@ -1,7 +1,18 @@
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Chat from "./pages/Chat";
+
 function App() {
   return (
     <div className="App">
-     App component
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/register" element={<Register/>}/>
+          <Route path="/" element={<Chat/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ public/src/pages/Register.jsx
@@ -0,0 +1,130 @@
+import styled from "styled-components"
+import { Link } from "react-router-dom";
+import logo from "../assets/logo.svg"
+
+const handleChange = (event) => {
+    
+  };
+
+const handleSubmit = (event) =>{
+
+}
+const Register = () => {
+    return ( 
+        <>
+        <FormContainer>
+        <form action="" onSubmit={(event) => handleSubmit(event)}>
+          <div className="brand">
+            <img src={logo} alt="logo" />
+            <h1>Chatify</h1>
+          </div>
+          <input
+            type="text"
+            placeholder="Username"
+            name="username"
+            onChange={(e) => handleChange(e)}
+          />
+          <input
+            type="email"
+            placeholder="Email"
+            name="email"
+            onChange={(e) => handleChange(e)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            onChange={(e) => handleChange(e)}
+          />
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            name="confirmPassword"
+            onChange={(e) => handleChange(e)}
+          />
+          <button type="submit">Create User</button>
+          <span>
+            Already have an account ? <Link to="/login">Login.</Link>
+          </span>
+        </form>
+      </FormContainer>
+        </>
+        
+     );
+}
+ 
+export default Register;
+
+
+
+const FormContainer = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 1rem;
+  align-items: center;
+  background-color: #131324;
+  .brand {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    justify-content: center;
+    img {
+      height: 5rem;
+    }
+    h1 {
+      color: white;
+      text-transform: uppercase;
+    }
+  }
+
+  form {
+    display: flex;
+    width:500px;
+    flex-direction: column;
+    gap: 2rem;
+    background-color: #00000076;
+    border-radius: 2rem;
+    padding: 3rem 5rem;
+  }
+  input {
+    background-color: transparent;
+    padding: 1rem;
+    border: 0.1rem solid #C822FF;
+    border-radius: 0.4rem;
+    color: white;
+    width: 100%;
+    font-size: 1rem;
+    &:focus {
+      border: 0.1rem solid #E6ABFF;
+      outline: none;
+    }
+  }
+  button {
+    background-color: #C822FF;
+    color: white;
+    padding: 1rem 2rem;
+    border: none;
+    font-weight: bold;
+    cursor: pointer;
+    border-radius: 0.4rem;
+    font-size: 1rem;
+    text-transform: uppercase;
+    &:hover {
+      outline: 2px solid #E6ABFF;
+    }
+  }
+  span {
+    color: white;
+    text-transform: uppercase;
+    font-size:14px;
+    text-align:center;
+    a {
+      color: #C822FF;
+      text-decoration: none;
+      font-weight: bold;
+    }
+  }
+`;
