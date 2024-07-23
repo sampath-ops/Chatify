a45564173cc59dda0925b93107e39db9b0bc2182
sampath-ops <sampathkaali002@gmail.com>
2024-07-24 00:08:05 +0530

    env temp files added and client domain updated in socket

--- server/index.js
+++ server/index.js
@@ -38,7 +38,7 @@ const server = app.listen(process.env.PORT, () =>
 
 const io = socket(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://chatifydev.vercel.app",
     credentials: true,
   },
 });
